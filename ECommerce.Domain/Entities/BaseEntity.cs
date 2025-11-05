4cc73f797cef74be8553ebc89f57d46f59b3b183
saramohamed512 <saramohamed12656@gmail.com>
2025-11-05 21:08:19 +0200

    Product Module

---
+++ ECommerce.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Domain.Entities
+{
+    public class BaseEntity<TKey>
+    {
+        public TKey Id { get; set; }= default!;
+        //GUID ID => String
+    }
+}
---
+++ ECommerce.Domain/Entities/ProductModule/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Domain.Entities.ProductModule
+{
+    public class Product: BaseEntity<int>
+    {
+        public string Name { get; set; } =null!;
+        public string Description { get; set; } =null!;
+        public string PictureUrl { get; set; } = null!;
+        public decimal Price { get; set; }
+
+        #region Relationships
+
+        //FK=> Fluent API
+        public int BrandId { get; set; }
+        public ProductBrand ProductBrands { get; set; } 
+
+        public int TypeId { get; set; }
+        public ProductType ProductTypes { get; set; }
+        #endregion
+    }
+}
