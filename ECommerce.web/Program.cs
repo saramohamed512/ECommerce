1099505ff3daf067080eaed043fa5f1b6ebd3fc1
saramohamed512 <saramohamed12656@gmail.com>
2025-11-08 17:44:08 +0200

    .

--- ECommerce.web/Program.cs
+++ ECommerce.web/Program.cs
@@ -13,7 +13,7 @@ namespace ECommerce.web
 {
     public class Program
     {
-        public static async void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
