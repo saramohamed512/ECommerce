b69343b4247f6f2eaa41184722090e30c1f324f3
saramohamed512 <saramohamed12656@gmail.com>
2025-11-16 19:57:20 +0200

    Basket Entities

---
+++ ECommerce.Domain/Entities/BasketModule/CustomerBasket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Domain.Entities.BasketModule
+{
+    public class CustomerBasket
+    {
+        public string Id { get; set; } = default;
+        public ICollection<BasketItem> Items { get; set; } = [];
+    }
+}
