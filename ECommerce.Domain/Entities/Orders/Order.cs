0cf07173276f02dd9f6a4eacfb22bba3f587347a
saramohamed512 <saramohamed12656@gmail.com>
2025-11-29 19:03:48 +0200

    Order Entities

---
+++ ECommerce.Domain/Entities/Orders/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Domain.Entities.Orders
+{
+    public class Order: BaseEntity<Guid>
+    {
+        public string UserEmil { get; set; } = null!;
+        public DateTimeOffset OrderDate { get; set; }= DateTimeOffset.Now;
+        public OrderAddress Address { get; set; } = null!;
+        [ForeignKey("DeliveryMethod")]
+        public int DeliveryMethodId { get; set; }
+        public DeliveryMethod DeliveryMethod { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+        public ICollection<OrderItem> Items { get; set; } = [];
+        public decimal Subtotal { get; set; }
+        public decimal GetTotal ()
+            => Subtotal + DeliveryMethod.Price;
+    }
+}
